483cb977e324a974bcccbc92c0143cb555ab6198
PomboMentir <deduardo@protonmail.com>
2025-04-30 15:48:19 -0300

    Update module configs.

--- babele-register.js
+++ babele-register.js
@@ -1,36 +1,11 @@
-function convertEnabled() {
-  return game.settings.get("DND5e-PTBR", "convert");
-}
-
-function setEncumbranceData() {
-  let convert = convertEnabled();
-  game.settings.set("dnd5e", "metricWeightUnits", convert);
-}
-
-
-
-
-Hooks.once('babele.init', (Babele) => {
-    
-    game.settings.register("DND5e-PTBR", "convert", {
-      name: "Conversão Automática",
-      hint: "Automaticamente aplica a conversão de medidas (pés-metros/milhas-quilômetros).",
-      scope: "world",
-      type: Boolean,
-      default: true,
-      config: true,
-      onChange: convert => {
-        setEncumbranceData();
-      }
-    });
-
-
-    Babele.register({
+Hooks.once('init', () => {
+  if(typeof Babele !== 'undefined'){
+    Babele.get().register({
       module: 'DND5e-PTBR',
       lang: 'pt-BR',
       dir: 'compendium'
     });
-
+  }
 
 });
 
